4ed51f9d9120cb37c4706f75f2eb3e650e2cc2de
KORLA GOUTHAM <96729391+KORLA2@users.noreply.github.com>
2022-09-24 21:40:02 +0530

    up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Header from './Header'
 import Body from './Body'
+let Li=({text,idx,index,handleclick})=>{
+  console.log(idx,index)
+return <li 
+onClick={()=>handleclick()}
+style={{ color: idx === index ? "green" : "red" }}>{text}</li>;
+
+}
+
 const App = () => {
+let [index,setindex]=useState('')
+let items=["hello","polo","Jolo"]
   return (
-    <div><Header/>
-    
-    <Body/>
+    <div>
+      <ul>
+        {items.map((e, idx) => (
+          <Li
+          key={idx}
+            text={e}
+index={index}
+idx={idx}
+            handleclick={()=>setindex(idx)}
+          />
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default App
